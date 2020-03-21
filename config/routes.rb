39369a48c182f02de7de605fe20c388b791031c0
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2020-03-22 08:38:00 +0900

    Revert "new create groups function"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
+  root "messages#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
+  resources :groups, only: [:new, :create]
 end
