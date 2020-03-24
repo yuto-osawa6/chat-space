8455edc28a4da76769fc00fd7bd17c8cc1564bf5
yuto-osawa6 <pooh.atgmw628@gmail.com>
2020-03-24 13:21:18 +0900

    create.all

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,64 +1,118 @@
-$(function(){ 
-function buildHTML(message){
-  if ( message.image ) {
-    var html =
-     `<div class="main__message-list__box">
-        <div class="uppaer-info">
-          <p class="uppaer-info__talker">
-            ${message.user_name}
-          </p>
-          <p class="uppaer-info__date">
-            ${message.created_at}
-          </p>
-        </div>
-        <p class="message-text"></p>
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        <img src=${message.image} >
-      </div>`
-    return html;
-  } else {
-    var html =
-     `<div class="main__message-list__box">
-        <div class="uppaer-info">
-          <p class="uppaer-info__talker">
-            ${message.user_name}
-          </p>
-          <p class="uppaer-info__date">
-            ${message.created_at}
-          </p>
-        </div>
-        <p class="message-text"></p>
-          <p class="lower-message__content">
-          ${message.content}
-          </p>
-        </div>`
-      return html;
+$(function() {
+  
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      //data-idが反映されるようにしている
+      var html = `<div class="main__message-list__box" data-message-id=` + message.id + `>` +
+        `<div class="uppaer-info">` +
+          `<p class="uppaer-info__talker">` +
+            message.user_name +
+          `</p>` +
+          `<p class="uppaer-info__date">` +
+            message.created_at +
+          `</p>` +
+        `</div>` +
+        `<p class="message-text">` +`</p>` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+      `</div>`
+    } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="main__message-list__box" data-message-id=` + message.id + `>` +
+        `<div class="uppaer-info">` +
+          `<p class="uppaer-info__talker">` +
+            message.user_name +
+          `</p>` +
+          `<p class="uppaer-info__date">` +
+            message.created_at +
+          `</p>` +
+        `</div>` +
+        `<p class="message-text">` +`</p>` +
+          `<p class="lower-message__content">` +
+            message.content +
+            `</p>` +
+      `</div>`
+    } else if (message.image) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="main__message-list__box" data-message-id=` + message.id + `>` +
+        `<div class="uppaer-info">` +
+          `<p class="uppaer-info__talker">` +
+            message.user_name +
+          `</p>` +
+          `<p class="uppaer-info__date">` +
+            message.created_at +
+          `</p>` +
+        `</div>` +
+        `<p class="message-text">` + `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</p>` +
+      `</div>`
     };
-  }
+    return html;
+  };
+
+
 
   $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-     .done(function(data){
-       var html = buildHTML(data);
-       $('.main__message-list').append(html);      
-       $('form')[0].reset();
-       $('.main__message-list').animate({ scrollTop: $('.main__message-list')[0].scrollHeight});
-     })
-     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+       .done(function(data){
+         var html = buildHTML(data);
+         $('.main__message-list').append(html);      
+         $('form')[0].reset();
+         $('.main__message-list').animate({ scrollTop: $('.main__message-list')[0].scrollHeight});
+       })
+       .fail(function() {
+        alert("メッセージ送信に失敗しました");
+    });
+    return false;
   });
-  return false;
-})
-});
+
+  
+  
+  
+    var reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      var last_message_id = $('.main__message-list__box:last').data("message-id");
+      console.log(last_message_id);
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.main__message-list').append(insertHTML);
+      $('.main__message-list').animate({ scrollTop: $('.main__message-list')[0].scrollHeight});
+      }
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+    }
+  });
